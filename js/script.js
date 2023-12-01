3310a2bfc0844742a67aea2c192df11d0ab51404
Ovidijus <ovidijus.e@gmail.com>
2023-12-01 20:14:17 +0200

    navigation update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-const navEl = document.querySelector(".nav");
-navEl.addEventListener("click", () => {
-  navEl.classList.toggle("is-active");
+const navEl = document.querySelector('.nav');
+navEl.addEventListener('click', () => {
+  navEl.classList.toggle('is-active');
+});
+
+const headerEl = document.querySelector('.header__container');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    headerEl.classList.add('is-scrolling');
+    navEl.classList.remove('is-active');
+  } else {
+    headerEl.classList.remove('is-scrolling');
+  }
 });
