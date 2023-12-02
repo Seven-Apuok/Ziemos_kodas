9a616a685968a57501c5d92f464397c873366cfd
Ovidijus <ovidijus.e@gmail.com>
2023-12-02 07:16:07 +0200

    adjusted gaps and nav button functionallity

--- js/script.js
+++ js/script.js
@@ -13,3 +13,28 @@ window.addEventListener('scroll', () => {
     headerEl.classList.remove('is-scrolling');
   }
 });
+
+// scroll Y axis adjustment after clicking nav button
+document.querySelectorAll('.nav__button').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const targetId = button.getAttribute('data-section');
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const headerHeight = document.querySelector('header').offsetHeight;
+      const offset = 30;
+
+      window.scrollTo({
+        top: targetElement.offsetTop - headerHeight - offset,
+        behavior: 'smooth',
+      });
+
+      const emailField = document.getElementById('schedule');
+      if (emailField) {
+        emailField.focus();
+      }
+    }
+  });
+});
