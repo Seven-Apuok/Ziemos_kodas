c188718367c505cfa456fc8db98775f01af5c0e3
Ovidijus <ovidijus.e@gmail.com>
2023-12-06 06:25:52 +0200

    checkmarks updated

--- js/script.js
+++ js/script.js
@@ -41,30 +41,3 @@ document.querySelectorAll('.nav__button').forEach((button) => {
     }
   });
 });
-
-// const navMenuEl = document.querySelector('.nav__menu');
-
-// const handleDocumentClick = (event) => {
-//   if (!navMenuEl.contains(event.target)) {
-//     navMenuEl.classList.remove('is-active');
-//   }
-// };
-
-// document.addEventListener('click', handleDocumentClick);
-// document.addEventListener('DOMContentLoaded', () => {
-//   const navMenuEl = document.querySelector('.nav__menu');
-//   const handleDocumentClick = (event) => {
-//     if (!navMenuEl.contains(event.target)) {
-//       navMenuEl.classList.remove('is-active');
-//     }
-//   };
-
-//   document.addEventListener('click', handleDocumentClick);
-// });
-
-// const handleDocumentClick = (event) => {
-//   console.log('Document Clicked');
-//   if (!navMenuEl.contains(event.target)) {
-//     navMenuEl.classList.remove('is-active');
-//   }
-// };
