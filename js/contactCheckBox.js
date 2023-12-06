d4a784687885101ae05700772c8f59ec3d73bb28
Darius <darius.mlecka@stud.techin.lt>
2023-12-06 22:49:17 +0100

    pixels perfect contacts tablet

--- js/contactCheckBox.js
+++ js/contactCheckBox.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const checkboxCustom = document.getElementById('checkboxCustom');
     const subscribeCheckbox = document.getElementById('subscribe');
 
-    // Pradinis SVG kodas
     const initialSvgCode = '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="13" viewBox="0 0 15 13" fill="none"><path d="M2 6.97348L5.17296 10.1464L13.3194 2" stroke="white" stroke-width="3"/></svg>';
 
     // Checkbox paspaudimo įvykio klausymas
